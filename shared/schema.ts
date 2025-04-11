904902575969d9042d0ad0bb40467300d9870fd7
JustDoIt-hub <78211928+JustDoIt-hub@users.noreply.github.com>
2025-04-11 15:28:39 +0530

    Update schema.ts

--- shared/schema.ts
+++ shared/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, serial, integer, boolean, json, timestamp, pgEnum,bigint } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, integer, boolean, json, timestamp, pgEnum,bigint,varchar,bytea } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 import 'dotenv/config';
