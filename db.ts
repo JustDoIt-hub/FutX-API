5d393303ba1625005b3aa35aa71d4bdcc6f9f246
JustDoIt-hub <78211928+JustDoIt-hub@users.noreply.github.com>
2025-04-25 13:14:14 +0530

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,18 +1,33 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import pg from 'pg';
-const { Pool } = pg;
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import pg from 'pg';
+// const { Pool } = pg;
 
+// // const pool = new Pool({
+// //   host: process.env.DB_HOST,
+// //   port: Number(process.env.DB_PORT),
+// //   user: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME,
+// // });
+
+
+// // // Export the Drizzle client
+// // export const db = drizzle(pool);
 // const pool = new Pool({
 //   host: process.env.DB_HOST,
 //   port: Number(process.env.DB_PORT),
 //   user: process.env.DB_USER,
 //   password: process.env.DB_PASSWORD,
 //   database: process.env.DB_NAME,
+//   ssl: {
+//     rejectUnauthorized: false, // 👈 this is the key
+//   },
 // });
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './shared/schema';
+const { Pool } = pg;
 
-
-// // Export the Drizzle client
-// export const db = drizzle(pool);
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
@@ -20,6 +35,8 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false, // 👈 this is the key
+    rejectUnauthorized: false,
   },
 });
+
+export const db = drizzle(pool, { schema });
