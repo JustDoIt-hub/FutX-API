bcb736e6176933d90a4cd9aceff12d95989adbb9
JustDoIt-hub <78211928+JustDoIt-hub@users.noreply.github.com>
2025-04-25 13:08:28 +0530

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,15 +1,25 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import pg from 'pg';
-const { Pool } = pg;
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import pg from 'pg';
+// const { Pool } = pg;
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+
+// // Export the Drizzle client
+// export const db = drizzle(pool);
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // 👈 this is the key
+  },
 });
-
-
-// Export the Drizzle client
-export const db = drizzle(pool);
